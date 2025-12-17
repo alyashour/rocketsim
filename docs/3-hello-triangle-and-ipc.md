ed9d34440f18171d216db30372033d496e72e2d2
Aly Ashour <alyashour1@gmail.com>
2025-12-17 00:26:35 -0500

    glfw, glad boilerplate

---
+++ docs/3-hello-triangle-and-ipc.md
@@ -0,0 +1,6 @@
+# Hello Triangle and IPC
+
+After wrangling GLFW and GLAD to work on my computer + some boiler plate to open a window it's time to start work on some specific functionality of the simulation.
+
+My renderer right now does a whole lot of nothing and my simulation does even less. Let's start by having the renderer draw a triangle to the screen, the color and position of which is decided by the simulation and communicated through shared memory.
+
--- src/visualizer/main.c
+++ src/visualizer/main.c
@@ -1,6 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void render(GLFWwindow*);
+void processInput(GLFWwindow* window);
+void errorCallback(int error, const char* description);
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
 
 int main() {
-    printf("Hello, Visualizer!\n");
-    return 0;
+    printf("Starting Visualizer...\n");
+
+    // glfw: init and configure
+    if (!glfwInit()) {
+        printf("Failed to load GLFW.\n");
+        exit(EXIT_FAILURE);
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // glfw create window
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "RocketSim Visualizer", NULL, NULL);
+    if (!window) {
+        printf("Failed to initialize GLFW window.\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Failed to load glad.\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // set error callback
+    glfwSetErrorCallback(errorCallback);
+ 
+    // sync to monitor
+    glfwSwapInterval(1);
+
+    // redner loop
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+
+        // render
+        render(window);
+
+        // glfw: swap buffers & poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // cleanup
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    // exit
+    exit(EXIT_SUCCESS);
+}
+
+void render(GLFWwindow*) { glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); }
+
+void processInput(GLFWwindow* window) {
+    // handle escape
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void errorCallback(int, const char* description) { fputs(description, stderr); }
+
+
+void framebufferSizeCallback(GLFWwindow*, int width, int height) {
+    glViewport(0, 0, width, height);
 }
